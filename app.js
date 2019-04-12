b6005e4651adbfb7c59d4e6ffdfd1952e3ae4654
Taran <Bond_J_0o7@hotmail.com>
2019-04-12 16:36:26 -0400

    4th question

--- app.js
+++ app.js
@@ -18,25 +18,38 @@ function onReady() {
     NEW_TODO_TEXT.value = '';
     // console.log(toDo);
     id++;
+    console.log('testasdfa: ' + toDo[0].title);
     renderTheUI();
   }
 
   function renderTheUI(){
     toDoList.textContent = '';
+    toDoListHistory.textContent = '';
 
     toDo.forEach(function(item){
-    const NEW_LI = document.createElement('li');
-    const CHECKBOX = document.createElement('input');
-    CHECKBOX.type = 'checkbox';
-    console.log(item);
+      const NEW_LI = document.createElement('li');
+      const NEW_LI2 = document.createElement('li');
+      const CHECKBOX = document.createElement('input');
+      CHECKBOX.type = 'checkbox';
+      const delete_button = document.createElement('button');
+      delete_button.className += 'mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent';
+      delete_button.textContent = 'Delete';
+      delete_button.addEventListener('click', event =>{
+        toDo = toDo.filter(function(item){
+          return item.id !== item.id;
+          // return;
+        })
+        alert('Item ID: ' + item.id + '     ' + 'toDo ID: ' + toDo[0].id);
+        renderTheUI();
+      });
 
-    NEW_LI.textContent = item.title;
-    NEW_LI.appendChild(CHECKBOX);
+      NEW_LI.textContent = item.title;
+      NEW_LI2.textContent = item.title;
+      NEW_LI.appendChild(CHECKBOX);
+      NEW_LI.appendChild(delete_button);
 
-    toDoList.appendChild(NEW_LI);
-    toDoListHistory.appendChild(NEW_LI);
-
-    console.log(NEW_LI);
+      toDoListHistory.appendChild(NEW_LI2);
+      toDoList.appendChild(NEW_LI);
     });
   }
   const ADD_TODO_FORM = document.getElementById('addToDoForm');
